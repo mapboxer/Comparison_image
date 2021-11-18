aadc83c317603fa1d1aa5275af960b9a2472e487
Murataev Denis <53305563+mapboxer@users.noreply.github.com>
2021-11-18 11:05:11 +0300

    Update Comparison_img.py

--- Comparison_img.py
+++ Comparison_img.py
@@ -14,19 +14,19 @@ from streamlit.ReportThread import add_report_ctx
 
 # U can put it in a separate file
 # This class can return value from thread
-class ThreadWithReturnValue(Thread):
-    def __init__(self, group=None, target=None, name=None, args=[], kwargs=None, *, daemon=None):
-        Thread.__init__(self, group, target, name, args, kwargs, daemon=daemon)
+# class ThreadWithReturnValue(Thread):
+#     def __init__(self, group=None, target=None, name=None, args=[], kwargs=None, *, daemon=None):
+#         Thread.__init__(self, group, target, name, args, kwargs, daemon=daemon)
 
-        self._return = None
+#         self._return = None
 
-    def run(self):
-        if self._target is not None:
-            self._return = self._target(*self._args, **self._kwargs)
+#     def run(self):
+#         if self._target is not None:
+#             self._return = self._target(*self._args, **self._kwargs)
 
-    def join(self):
-        Thread.join(self)
-        return self._return
+#     def join(self):
+#         Thread.join(self)
+#         return self._return
 # This part of code responsible for processing and displaying images of a specific thread
 # (requires refracting)
 def comparison_img(before, after):
@@ -104,10 +104,10 @@ if uploaded_file_two:
 # Due to the fact that it is a server and performs constant reading (loop),
 # this design excludes the creation of a thread before the images are loaded
 # (there may be a more elegant solution, but I don’t know it yet)
-try:
-    thread = ThreadWithReturnValue(target=comparison_img, args=(before, after))
-    add_report_ctx(thread)
-    thread.start()
-    thread.join()
-except:
-    pass
+# try:
+#     thread = ThreadWithReturnValue(target=comparison_img, args=(before, after))
+#     add_report_ctx(thread)
+#     thread.start()
+#     thread.join()
+# except:
+#     pass
